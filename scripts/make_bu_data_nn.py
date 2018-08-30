4b845d1ad2085e21c571943863a8e896f62b8b10
Guandao Yang <gy46@cornell.edu>
2018-08-30 02:39:12 +0000

    [add]

--- scripts/make_bu_data_nn.py
+++ scripts/make_bu_data_nn.py
@@ -25,17 +25,23 @@ csv.field_size_limit(sys.maxsize)
 
 FIELDNAMES = ['image_id', 'image_w','image_h','num_boxes', 'boxes', 'features']
 infiles = ['trainval/karpathy_test_resnet101_faster_rcnn_genome.tsv',
-          'trainval/karpathy_val_resnet101_faster_rcnn_genome.tsv',\
-          'trainval/karpathy_train_resnet101_faster_rcnn_genome.tsv.0', \
+           'trainval/karpathy_val_resnet101_faster_rcnn_genome.tsv',\
+           'trainval/karpathy_train_resnet101_faster_rcnn_genome.tsv.0', \
            'trainval/karpathy_train_resnet101_faster_rcnn_genome.tsv.1']
+
+# For testing
+# infiles = ['test2014_resnet101_faster_rcnn_genome_36.tsv']
+
 
 os.makedirs(args.output_dir+'_att')
 os.makedirs(args.output_dir+'_fc')
 os.makedirs(args.output_dir+'_box')
+os.makedirs(args.output_dir+'_nn')
 
 imgid = []
 chipid = [] # str(imgid + '_' + ctr)
 feats = []
+fc_feats = []
 for infile in infiles:
     print('Reading ' + infile)
     with open(os.path.join(args.downloaded_feats, infile), "r+b") as tsv_in_file:
@@ -53,12 +59,16 @@ for infile in infiles:
                 imgid.append(item['image_id'])
                 chipid.append("%d_%d"%(item['image_id'], ctr))
             feats.append(item['features'])
+            fc_feats.append(item['features'].mean(0))
             np.savez_compressed(os.path.join(args.output_dir+'_att', str(item['image_id'])), feat=item['features']) # 32xD
-            np.save(os.path.join(args.output_dir+'_fc', str(item['image_id'])), item['features'].mean(0)) # 32xD -> D
+            np.save(os.path.join(args.output_dir+'_fc', str(item['image_id'])),
+                item['features'].mean(0)) # 32xD -> D
             np.save(os.path.join(args.output_dir+'_box', str(item['image_id'])), item['boxes'])
 
 feats = np.concatenate(feats, axis=0)
+fc_feats = np.stack(fc_feats, axis=0)
 np.savez_compressed(os.path.join(args.output_dir+"_nn", "feat.npz"), feats)
+np.savez_compressed(os.path.join(args.output_dir+"_nn", "fc_feat.npz"), fc_feats)
 infos = {
         'imgid' : imgid,
         'chipid' : chipid
